8461e2c37c0cd36ffb98499f8c1a9aa8cacca5ea
kitakou0313 <kitakou0313@gmail.com>
2021-07-07 14:31:46 +0000

    Add data load proc

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from os import PathLike
 from utils import dataparser
+from typing import List
 
+DATAPATH = "data/"
 if __name__ == "__main__":
-    inputDatas = dataparser.parseInputData("data/hira0_00L.dat")
-    for data in inputDatas:
-        for line in data.data:
-            print(line)
+    writer1TrainDatas:List[dataparser.CharData] = []
+
+    for dataNum in range(0, 20):
+        path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "L.dat"
+        writer1TrainDatas += dataparser.parseInputData(path)
+
+    writer1TestDatas:List[dataparser.CharData] = []
+
+    for dataNum in range(0, 20):
+        path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "T.dat"
+        writer1TestDatas += dataparser.parseInputData(path)
+
+    writer2TrainDatas:List[dataparser.CharData] = []
+
+    for dataNum in range(0, 20):
+        path = DATAPATH + "hira1_" + str(dataNum).zfill(2) + "L.dat"
+        writer2TrainDatas += dataparser.parseInputData(path)
+
+    writer2TestDatas:List[dataparser.CharData] = []
+
+    for dataNum in range(0, 20):
+        path = DATAPATH + "hira1_" + str(dataNum).zfill(2) + "T.dat"
+        writer2TestDatas += dataparser.parseInputData(path)
+
+    pass
