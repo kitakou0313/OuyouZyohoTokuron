fadbcaaa03a64d39295c73d0f73a510810bb2e94
kitakou0313 <kitakou0313@gmail.com>
2021-07-09 15:16:19 +0000

    Add arg to neural net const

--- network/neuralNet.py
+++ network/neuralNet.py
@@ -7,7 +7,7 @@ class NeuralNet(object):
     出力:20次元ベクトル（文字の種類と同数）
 
     """
-    def __init__(self) -> None:
+    def __init__(self, numOfMiddleLayers:int) -> None:
         self.layer = []
 
     def train(self) -> None:
