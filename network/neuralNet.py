1e17a112bd884941bcde31cac708b6ad71d42723
kitakou0313 <kitakou0313@gmail.com>
2021-07-09 15:01:09 +0000

    Write comment for NN

--- network/neuralNet.py
+++ network/neuralNet.py
@@ -1,6 +1,11 @@
 class NeuralNet(object):
     """
     NeuralNetworkクラス
+
+    入力:64次元ベクトル（メッシュ特徴の次元数）
+    中間層:適当
+    出力:20次元ベクトル（文字の種類と同数）
+
     """
     def __init__(self) -> None:
         self.layer = []
