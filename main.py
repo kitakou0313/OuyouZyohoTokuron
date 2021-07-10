47684a6d788cc88bfce28cb619ce90f79eac95d2
kitakou0313 <kitakou0313@gmail.com>
2021-07-10 08:59:04 +0000

    Add comment

--- main.py
+++ main.py
@@ -3,31 +3,36 @@ from network import neuralNet
 from typing import List
 
 DATAPATH = "data/"
+MIDDLE_LAYER_WIDTH = 50
 if __name__ == "__main__":
+    print("データの読み込み")
+    print("筆者1:train")
     writer1TrainDatas:List[dataparser.CharData] = []
-
     for dataNum in range(0, 20):
         path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "L.dat"
         writer1TrainDatas += dataparser.parseInputData(path,dataNum)
 
+    print("筆者1:test")
     writer1TestDatas:List[dataparser.CharData] = []
-
     for dataNum in range(0, 20):
         path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "T.dat"
         writer1TestDatas += dataparser.parseInputData(path,dataNum)
 
+    print("筆者2:train")
     writer2TrainDatas:List[dataparser.CharData] = []
-
     for dataNum in range(0, 20):
         path = DATAPATH + "hira1_" + str(dataNum).zfill(2) + "L.dat"
         writer2TrainDatas += dataparser.parseInputData(path,dataNum)
 
+    print("筆者2:test")
     writer2TestDatas:List[dataparser.CharData] = []
-
     for dataNum in range(0, 20):
         path = DATAPATH + "hira1_" + str(dataNum).zfill(2) + "T.dat"
         writer2TestDatas += dataparser.parseInputData(path,dataNum)
 
-    model = neuralNet.NeuralNet()
+    print("データ読み込み完了")
+    print("--------------------")
+
+    model = neuralNet.NeuralNet(MIDDLE_LAYER_WIDTH)
 
     pass
