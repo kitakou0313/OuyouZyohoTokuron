d26c14cb95c6b9e0b3bf49b3f3f0c81be32c6eba
kitakou0313 <kitakou0313@gmail.com>
2021-07-10 14:39:17 +0000

    Chagng data name

--- main.py
+++ main.py
@@ -5,42 +5,36 @@ from typing import List
 DATAPATH = "data/"
 if __name__ == "__main__":
     print("データの読み込み")
+    print("筆者0:train")
+    writer0TrainDatas:List[dataparser.CharData] = []
+    for dataNum in range(0, 20):
+        path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "L.dat"
+        writer0TrainDatas += dataparser.parseInputData(path,dataNum)
+
+    print("筆者0:test")
+    writer0TestDatas:List[dataparser.CharData] = []
+    for dataNum in range(0, 20):
+        path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "T.dat"
+        writer0TestDatas += dataparser.parseInputData(path,dataNum)
+
     print("筆者1:train")
     writer1TrainDatas:List[dataparser.CharData] = []
     for dataNum in range(0, 20):
-        path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "L.dat"
+        path = DATAPATH + "hira1_" + str(dataNum).zfill(2) + "L.dat"
         writer1TrainDatas += dataparser.parseInputData(path,dataNum)
 
     print("筆者1:test")
     writer1TestDatas:List[dataparser.CharData] = []
     for dataNum in range(0, 20):
-        path = DATAPATH + "hira0_" + str(dataNum).zfill(2) + "T.dat"
-        writer1TestDatas += dataparser.parseInputData(path,dataNum)
-
-    print("筆者2:train")
-    writer2TrainDatas:List[dataparser.CharData] = []
-    for dataNum in range(0, 20):
-        path = DATAPATH + "hira1_" + str(dataNum).zfill(2) + "L.dat"
-        writer2TrainDatas += dataparser.parseInputData(path,dataNum)
-
-    print("筆者2:test")
-    writer2TestDatas:List[dataparser.CharData] = []
-    for dataNum in range(0, 20):
         path = DATAPATH + "hira1_" + str(dataNum).zfill(2) + "T.dat"
-        writer2TestDatas += dataparser.parseInputData(path,dataNum)
+        writer1TestDatas += dataparser.parseInputData(path,dataNum)
 
     print("データ読み込み完了")
     print("--------------------")
+    print("1.筆記者0の学習用データを用いて、ニューラルネットの学習を行なえ。")
+    modelTrainedWriter0 = neuralNet.NeuralNet()
+    neuralNet.trainModel(modelTrainedWriter0, writer0TrainDatas)
 
-    print("ネットワークの初期化")
-    model = neuralNet.NeuralNet()
 
-    print("--------------------")
-    print("学習開始")
-
-    print("筆者0のデータで学習")
-
-    for inputData in writer1TrainDatas:
-        model.train(inputData)
 
     
