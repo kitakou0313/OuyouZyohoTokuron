4b5e3bf510944cd5728df74f6fdd53a9243818fe
kitakou0313 <kitakou0313@gmail.com>
2021-07-11 12:42:23 +0000

    Change train end diff

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     np.random.seed(seed=20210712)
     modelTrainedWriter0 = neuralNet.NeuralNet(nnParams)
 
-    TRAIN_LIMIT_L2NORM_DIFF = 0.00001
+    TRAIN_LIMIT_L2NORM_DIFF = 0.0001
     neuralNet.trainModel(modelTrainedWriter0, writer0TrainDatas[::50], TRAIN_LIMIT_L2NORM_DIFF)
 
     print("--------------------")
